aee7cdb3a5a4517126c101da4483b36455693236
Rodolfo Puig <rodolfo@puig.io>
2013-09-23 23:13:17 -0400

    Misc update
    Cleaning up the rake Configuration class

--- files/rake/configuration.rb
+++ files/rake/configuration.rb
@@ -1,8 +1,8 @@
 class Configuration
-  attr_accessor :hostname, :username, :private_key, :client_id, :api_key
+  attr_accessor :hostname, :private_key, :client_id, :api_key
 
   def initialize()
-    @hostname = "digitalocean-master"
+    @hostname = "vps-digitalocean"
     @private_key = "~/.ssh/id_rsa"
     @client_id = nil
     @api_key = nil
